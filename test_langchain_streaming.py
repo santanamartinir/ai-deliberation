ee28a0ec7369ee23f3b65dedc630e9412584f368
santanamartinir <irenesantanamartin01@gmail.com>
2026-05-14 12:05:26 +0200

    rm files moved to test

--- test_langchain_streaming.py
+++
@@ -1,7 +0,0 @@
-from langchain_community.llms import Ollama
-
-llm = Ollama(model="mistral")
-
-for chunk in llm.stream("The first man on the summit of Mount Everest, the highest peak on Earth, was ..."):
-    print(chunk, end="", flush=True)
-print()
--- test_ollama.py
+++
@@ -1,9 +0,0 @@
-from langchain_community.llms import Ollama
-
-llm = Ollama(model="mistral")
-
-# response = llm.invoke("Tell me a joke")
-# print(response)
-
-response = llm.invoke("The first man on the summit of Mount Everest, the highest peak on Earth, was ...")
-print(response)
